cb6f3197a92f3c356a51d23cb92857778e350881
Luxon JEAN-PIERRE <luxon.jean.pierre@gmail.com>
2016-11-02 16:39:01 +0100

    Prototype(NavigationDrawerFragment): implemented updateContent

--- app/src/main/java/com/pl/multicast/kraken/NavigationDrawerFragment.java
+++ app/src/main/java/com/pl/multicast/kraken/NavigationDrawerFragment.java
@@ -99,19 +99,26 @@ public class NavigationDrawerFragment extends Fragment {
         });
         mDrawerListView.setAdapter(new ArrayAdapter<String>(
                 getActionBar().getThemedContext(),
-                android.R.layout.simple_list_item_activated_1,
+                android.R.layout.simple_list_item_activated_2,
                 android.R.id.text1,
-                new String[]{
-                        getString(R.string.title_section1),
-                        getString(R.string.title_section2),
-                        getString(R.string.title_section3),
-                }));
+                new String[]{"No deveice"}));
+
         mDrawerListView.setItemChecked(mCurrentSelectedPosition, true);
         return mDrawerListView;
     }
 
     public boolean isDrawerOpen() {
         return mDrawerLayout != null && mDrawerLayout.isDrawerOpen(mFragmentContainerView);
+    }
+
+
+    /// Update the content of the fragment for each connected device
+    public void updateContent(String [] content)
+    {
+        mDrawerListView.setAdapter(new ArrayAdapter<String>(
+                getActionBar().getThemedContext(),
+                android.R.layout.simple_list_item_activated_2,
+                android.R.id.text1, content));
     }
 
     /**
