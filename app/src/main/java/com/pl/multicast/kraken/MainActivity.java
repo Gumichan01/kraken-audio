06a48dee71d5db3b02450bc9d31cd57d9103a21e
Luxon JEAN-PIERRE <luxon.jean.pierre@gmail.com>
2016-11-02 17:20:32 +0100

    MainActivity: new todo list

--- app/src/main/java/com/pl/multicast/kraken/MainActivity.java
+++ app/src/main/java/com/pl/multicast/kraken/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -33,5 +34,11 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    public void mix(View v)
+    {
+        /// @// TODO: 02/11/2016 Launch GraphActivity sending the username 
     }
 }
