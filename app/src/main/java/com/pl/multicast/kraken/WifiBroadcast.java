c3d9e047e2961d9c187794bd14afb247d5768b8b
kenny <Kenrabari@yahoo.fr>
2016-11-04 18:49:35 +0100

    prototype: discoverPeers

--- app/src/main/java/com/pl/multicast/kraken/WifiBroadcast.java
+++ app/src/main/java/com/pl/multicast/kraken/WifiBroadcast.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Looper;
+import android.util.Log;
 
 /**
  * Created by Luxon on 04/11/2016.
@@ -19,6 +20,17 @@ public class WifiBroadcast extends BroadcastReceiver{
         super();
         p2p = (WifiP2pManager) graph.getSystemService(Context.WIFI_P2P_SERVICE);
         chan = p2p.initialize(graph, graph.getMainLooper(), null);
+
+        p2p.discoverPeers(chan, new WifiP2pManager.ActionListener(){
+            @Override
+            public void onSuccess(){
+                Log.i("activity","SUCCESS");
+            }
+            @Override
+            public void onFailure(int reasonCode){
+                Log.i("activity","FAIL");
+            }
+        });
     }
 
     @Override
