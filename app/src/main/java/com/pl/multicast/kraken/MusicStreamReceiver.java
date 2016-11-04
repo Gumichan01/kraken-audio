0efcf3bef2dc475b41f9439142a06817d58b5be2
Luxon JEAN-PIERRE <luxon.jean.pierre@gmail.com>
2016-11-04 15:10:29 +0100

    MusicStream: skeleton (-.-)

--- app/src/main/java/com/pl/multicast/kraken/MusicStreamReceiver.java
+++ app/src/main/java/com/pl/multicast/kraken/MusicStreamReceiver.java
@@ -3,9 +3,21 @@ package com.pl.multicast.kraken;
 import java.util.Observable;
 
 /**
- * Created by Luxon on 04/11/2016.
+ *  Created by Luxon on 04/11/2016.
+ *
+ *  Linked with the list of readers
+ *
+ *  @// TODO: 04/11/2016 Establish the research to find any device
+ *  @// TODO: 04/11/2016 For each found device update every observers
+ *          (this assume that the observers have been added)
  */
 public class MusicStreamReceiver extends Observable{
 
 
+    public MusicStreamReceiver(NavDrawer nav){
+
+        /// @// TODO: 04/11/2016 Add the navigation drawer to the list of observers
+        /// @// TODO: 04/11/2016 Create the WiFiReceiver
+    }
+        
 }
--- app/src/main/java/com/pl/multicast/kraken/MusicStreamSender.java
+++ app/src/main/java/com/pl/multicast/kraken/MusicStreamSender.java
@@ -4,7 +4,18 @@ import java.util.Observable;
 
 /**
  * Created by Luxon on 04/11/2016.
+ *
+ *  Linked with the list of senders
+ *
+ *  @// TODO: 04/11/2016 Establish the research to find any device
+ *  @// TODO: 04/11/2016 For each found device update every observers (this assume that the observers have been added)
+ *
  */
 public class MusicStreamSender extends Observable {
+
+    public MusicStreamReceiver(NavDrawer nav){
+
+        /// @// TODO: 04/11/2016 Add the navigation drawer to the list of observers
+    }
 
 }
