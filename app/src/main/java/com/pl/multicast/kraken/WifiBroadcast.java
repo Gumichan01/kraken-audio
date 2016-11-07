23d8bea4339861a51699355eb681ee0552812806
Luxon JEAN-PIERRE <luxon.jean.pierre@gmail.com>
2016-11-07 16:43:49 +0100

    WifiBroadcast: Connection

--- app/src/main/java/com/pl/multicast/kraken/WifiBroadcast.java
+++ app/src/main/java/com/pl/multicast/kraken/WifiBroadcast.java
@@ -3,11 +3,18 @@ package com.pl.multicast.kraken;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.NetworkInfo;
+import android.net.wifi.WpsInfo;
+import android.net.wifi.p2p.WifiP2pConfig;
 import android.net.wifi.p2p.WifiP2pDevice;
 import android.net.wifi.p2p.WifiP2pDeviceList;
+import android.net.wifi.p2p.WifiP2pInfo;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.util.Log;
+import android.widget.Toast;
 
+import java.net.Inet4Address;
+import java.net.InetAddress;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -37,10 +44,10 @@ public class WifiBroadcast extends BroadcastReceiver {
                 peers.clear();
                 peers.addAll(peerList.getDeviceList());
 
-                if(peers.isEmpty())
-                    Log.i(TAG,"No peers");
+                if (peers.isEmpty())
+                    Log.i(TAG, "No peers");
                 else
-                    Log.i(TAG,"You have " + peers.size() + " peers");
+                    Log.i(TAG, "You have " + peers.size() + " peers");
 
             }
         };
@@ -48,12 +55,12 @@ public class WifiBroadcast extends BroadcastReceiver {
         p2p.discoverPeers(chan, new WifiP2pManager.ActionListener() {
             @Override
             public void onSuccess() {
-                Log.i(TAG, "SUCCESS");
+                Log.i(TAG, "discoverPeers() → SUCCESS");
             }
 
             @Override
             public void onFailure(int reasonCode) {
-                Log.i(TAG, "FAIL");
+                Log.i(TAG, "discoverPeers() → FAILURE");
             }
         });
     }
@@ -77,14 +84,26 @@ public class WifiBroadcast extends BroadcastReceiver {
             }
         } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
 
-            if(p2p != null)
-                p2p.requestPeers(chan,plisten);
+            if (p2p != null)
+                p2p.requestPeers(chan, plisten);
 
             Log.i(TAG, "Peer list changed");
         } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
 
             Log.i(TAG, "Connection state changed");
 
+            if(p2p != null){
+
+                NetworkInfo netInfo = (NetworkInfo) intent.getParcelableExtra(WifiP2pManager.EXTRA_NETWORK_INFO);
+
+                if(netInfo.isConnected()){
+
+                    Log.i(TAG, "CONNECTED");
+                }
+                else {
+                    Log.i(TAG, "NOT CONNECTED");
+                }
+            }
 
         } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {
 
@@ -93,9 +112,55 @@ public class WifiBroadcast extends BroadcastReceiver {
 
             if (wd != null)
                 Log.i(TAG, wd.deviceName + " " + wd.deviceAddress);
-            /// @// TODO: 07/11/2016 Send the result to the activity (mac address)
         }
 
     }
 
+    // Connection to a device
+    public void connect() {
+
+        // for this example, we just test the connection with the fisrt device
+        WifiP2pDevice device = (WifiP2pDevice) peers.get(0);
+
+        WifiP2pConfig p2pconfig = new WifiP2pConfig();
+        p2pconfig.deviceAddress = device.deviceAddress;
+        p2pconfig.wps.setup = WpsInfo.PBC;
+
+        p2p.connect(chan, p2pconfig, new WifiP2pManager.ActionListener() {
+            @Override
+            public void onSuccess() {
+                Log.i(TAG, "connect() → SUCCESS");
+            }
+
+            @Override
+            public void onFailure(int reason) {
+                Log.i(TAG, "connect() → FAILURE");
+                Toast.makeText(graph, "Connection failed. Retry", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+    public void onConnectionInfoAvailable(final WifiP2pInfo info){
+
+        //InetAddress groupOwnerAddress = info.groupOwnerAddress;
+
+        // After the group negotiation, we can determine the group owner.
+        if (info.groupFormed && info.isGroupOwner) {
+
+            /// Serveur
+            // Do whatever tasks are specific to the group owner.
+            // One common case is creating a server thread and accepting
+            // incoming connections.
+
+        } else if (info.groupFormed) {
+
+            /// Client
+            // The other device acts as the client. In this case,
+            // you'll want to create a client thread that connects to the group
+            // owner.
+        }
+
+
+    }
 }
