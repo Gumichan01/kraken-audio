951603d3d35bb75f288f6fa0afe7fa3675170e41
Luxon JEAN-PIERRE <luxon.jean.pierre@gmail.com>
2016-11-07 16:03:20 +0100

    WifiBroadcast: completed onReceive()

--- app/src/main/java/com/pl/multicast/kraken/WifiBroadcast.java
+++ app/src/main/java/com/pl/multicast/kraken/WifiBroadcast.java
@@ -8,15 +8,21 @@ import android.net.wifi.p2p.WifiP2pDeviceList;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.util.Log;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 /**
  * Created by Luxon on 04/11/2016.
  */
 public class WifiBroadcast extends BroadcastReceiver {
 
+    public static final String TAG = "WIFI-DIRECT_STATUS";
     private WifiP2pManager p2p;
     private WifiP2pManager.Channel chan;
     private GraphActivity graph;
+    private List peers = new ArrayList();
+    private WifiP2pManager.PeerListListener plisten;
 
     public WifiBroadcast(WifiP2pManager wp2p, WifiP2pManager.Channel ch, GraphActivity g) {
         super();
@@ -24,16 +30,30 @@ public class WifiBroadcast extends BroadcastReceiver {
         p2p = wp2p;
         chan = ch;
         graph = g;
+        plisten = new WifiP2pManager.PeerListListener() {
+            @Override
+            public void onPeersAvailable(WifiP2pDeviceList peerList) {
+
+                peers.clear();
+                peers.addAll(peerList.getDeviceList());
+
+                if(peers.isEmpty())
+                    Log.i(TAG,"No peers");
+                else
+                    Log.i(TAG,"You have " + peers.size() + " peers");
+
+            }
+        };
 
         p2p.discoverPeers(chan, new WifiP2pManager.ActionListener() {
             @Override
             public void onSuccess() {
-                Log.i("WIFI-DIRECT_STATUS", "SUCCESS");
+                Log.i(TAG, "SUCCESS");
             }
 
             @Override
             public void onFailure(int reasonCode) {
-                Log.i("WIFI-DIRECT_STATUS", "FAIL");
+                Log.i(TAG, "FAIL");
             }
         });
     }
@@ -49,27 +69,30 @@ public class WifiBroadcast extends BroadcastReceiver {
 
             if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
 
-                Log.i("WIFI-DIRECT_STATUS", "Wi-Fi P2P is activated");
+                Log.i(TAG, "Wi-Fi P2P is activated");
 
             } else if (state == WifiP2pManager.WIFI_P2P_STATE_DISABLED) {
 
-                Log.i("WIFI-DIRECT_STATUS", "Wi-Fi P2P is not activated");
+                Log.i(TAG, "Wi-Fi P2P is not activated");
             }
         } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
 
-            Log.i("WIFI-DIRECT_STATUS", "Peer list changed");
+            if(p2p != null)
+                p2p.requestPeers(chan,plisten);
+
+            Log.i(TAG, "Peer list changed");
         } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
 
-            Log.i("WIFI-DIRECT_STATUS", "Connection state changed");
+            Log.i(TAG, "Connection state changed");
 
 
         } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {
 
-            Log.i("WIFI-DIRECT_STATUS", "Device");
+            Log.i(TAG, "Device");
             WifiP2pDevice wd = (WifiP2pDevice) intent.getParcelableExtra(WifiP2pManager.EXTRA_WIFI_P2P_DEVICE);
 
-            if(wd != null)
-                Log.i("WIFI-DIRECT_STATUS",wd.deviceName + " " + wd.deviceAddress);
+            if (wd != null)
+                Log.i(TAG, wd.deviceName + " " + wd.deviceAddress);
             /// @// TODO: 07/11/2016 Send the result to the activity (mac address)
         }
 
