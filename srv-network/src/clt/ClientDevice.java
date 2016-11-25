e6784fb4d24bc84037a979b085b2c00242a99406
kenny <Kenrabari@yahoo.fr>
2016-11-25 13:03:35 +0100

    comment

--- srv-network/src/clt/ClientDevice.java
+++ srv-network/src/clt/ClientDevice.java
@@ -1,5 +1,34 @@
 package clt;
 
 public class ClientDevice {
-
+	public ClientDevice(){
+		
+	}
+	
+	// Création d'un groupe:
+	
+    //CGRP nom_groupe nom_appareil ip_addr port
+    /// CGRP : Create GRouP
+    /*
+    NB: *ip_addr* et *port* correspondent respectivement à l'adresse IP
+    et au n° de port de l'appareil.
+    */
+	
+	// Rejoindre un groupe spécifique:
+	
+    //JGRP nom_groupe nom_appareil ip_addr port
+    /// JGRP : Join GRouP
+    /*
+    NB: *ip* et *port* correspondent respectivement à l'adresse IP
+    et au n° de port de l'appareil.
+    */
+    
+	// Avoir la liste des groupes:
+	
+	//GRPL    /// GRPL : GRouP List
+	
+	// (Supprimer un groupe) → pour l'instant on ne met pas, on verra si besoin.
+	
+	// Quitter un groupe:
+	//QGRP    /// QGRP : Quit GRouP
 }
--- srv-network/src/srv/DirectoryServer.java
+++ srv-network/src/srv/DirectoryServer.java
@@ -1,5 +1,14 @@
 package srv;
 
-public class DirectoryServer {
+import java.util.Hashtable;
 
+
+public class DirectoryServer {
+	
+	private Hashtable<String, GroupInfo> groups;
+	
+	public DirectoryServer(){
+		groups = new Hashtable<>();
+	}
+	
 }
