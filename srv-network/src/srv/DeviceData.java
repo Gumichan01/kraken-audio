cb4fb73b2f4ee25d44511e598f7f17be1c46b762
kenny <Kenrabari@yahoo.fr>
2016-11-25 13:02:39 +0100

    DeviceData: definition

--- srv-network/src/srv/DeviceData.java
+++ srv-network/src/srv/DeviceData.java
@@ -1,5 +1,12 @@
 package srv;
 
 public class DeviceData {
-
+	
+	private String ipaddr;
+	private int port;
+	
+	public DeviceData(String ipaddr, int port){
+		this.ipaddr = ipaddr;
+		this.port = port;
+	}
 }
