03e900c4d027be995fdc6d192528931be39d73d4
kenny <Kenrabari@yahoo.fr>
2016-11-25 13:01:52 +0100

    GroupInfo: definition

--- srv-network/src/srv/GroupInfo.java
+++ srv-network/src/srv/GroupInfo.java
@@ -1,5 +1,19 @@
 package srv;
 
-public class GroupInfo {
+import java.util.Hashtable;
 
+public class GroupInfo {
+	
+	private String name;
+	private Hashtable<String, DeviceData> devices;
+	
+	public GroupInfo(String name){
+		this.name = name;
+		devices = new Hashtable<>();
+	}
+	
+	public boolean addDevice(String key, DeviceData data){
+		devices.put(key, data);
+		return true;
+	}
 }
