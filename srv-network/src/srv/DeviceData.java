22494aec820d4ddf0b8c355542ea4f9b7986a50a
Luxon JEAN-PIERRE <luxon.jean.pierre@gmail.com>
2016-11-27 17:49:02 +0100

    DeviceData: added getters + toString().

--- srv-network/src/srv/DeviceData.java
+++ srv-network/src/srv/DeviceData.java
@@ -1,12 +1,28 @@
 package srv;
 
 public class DeviceData {
-	
+
 	private String ipaddr;
 	private int port;
-	
-	public DeviceData(String ipaddr, int port){
+
+	public DeviceData(String ipaddr, int port) {
 		this.ipaddr = ipaddr;
 		this.port = port;
 	}
+
+	public String getAddr() {
+
+		return ipaddr;
+	}
+
+	public int getPort() {
+
+		return port;
+	}
+
+	public String toString() {
+
+		return "" + ipaddr + ":" + port;
+	}
+
 }
