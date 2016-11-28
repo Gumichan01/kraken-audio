322173aab02a8f7993f51bc968d99a112880dce8
kenny <Kenrabari@yahoo.fr>
2016-11-28 15:54:58 +0100

    ClientDevice: constructor

--- srv-network/src/clt/ClientDevice.java
+++ srv-network/src/clt/ClientDevice.java
@@ -1,34 +1,42 @@
 package clt;
 
-public class ClientDevice {
-	public ClientDevice() {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
+
+public class ClientDevice {
+	private Socket socket;
+	private BufferedReader reader;
+	private PrintWriter writer;
+	private String device_name;
+	private InetSocketAddress ipaddr;
+		
+	public ClientDevice(String name, String addr, int port) {
+		this.device_name = name;
+		this.ipaddr = new InetSocketAddress(addr, port);
+		try {
+			socket = new Socket(InetAddress.getByName("localhost"), 2048);
+			reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	// Création d'un groupe:
-
-	// CGRP nom_groupe nom_appareil ip_addr port
-	// / CGRP : Create GRouP
-	/*
-	 * NB: *ip_addr* et *port* correspondent respectivement à l'adresse IP et au
-	 * n° de port de l'appareil.
-	 */
-
 	// Rejoindre un groupe spécifique:
-
-	// JGRP nom_groupe nom_appareil ip_addr port
-	// / JGRP : Join GRouP
-	/*
-	 * NB: *ip* et *port* correspondent respectivement à l'adresse IP et au n°
-	 * de port de l'appareil.
-	 */
-
 	// Avoir la liste des groupes:
-
-	// GRPL /// GRPL : GRouP List
-
-	// (Supprimer un groupe) → pour l'instant on ne met pas, on verra si besoin.
-
+	// Avoir la liste des appareils d'un groupe donné:
 	// Quitter un groupe:
-	// QGRP /// QGRP : Quit GRouP
 }
