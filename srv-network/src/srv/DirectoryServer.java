3ab6b80ef435a2f34be864b1c11ac63421903677
Luxon JEAN-PIERRE <luxon.jean.pierre@gmail.com>
2016-12-16 12:58:44 +0100

    server: clean the code

--- srv-network/src/srv/DirectoryServer.java
+++ srv-network/src/srv/DirectoryServer.java
@@ -6,11 +6,11 @@ import java.net.Socket;
 import java.util.Hashtable;
 import java.util.Iterator;
 
-import clt.ClientDevice;
 
 public class DirectoryServer {
 
 	private static final int BUFFER_SIZE = 1024;
+	private static final int SERVER_PORT = 8080;
 
 	ServerSocket srvsock;
 	char[] buffer;
@@ -25,7 +25,7 @@ public class DirectoryServer {
 
 		try {
 
-			srvsock = new ServerSocket(2048);
+			srvsock = new ServerSocket(SERVER_PORT);
 			buffer = new char[BUFFER_SIZE];
 
 			System.out.println("Server @"
@@ -120,55 +120,11 @@ public class DirectoryServer {
 		return groups.size();
 	}
 
-	// / * Uncomment this block in order to test the class
-	public static void main(String[] args) {
+	// Uncomment this block in order to test the class
+	//public static void main(String[] args) {
 
 		// PRODUCTION CODE
-		new DirectoryServer().launch();
-
-		/** For testing the server (DO NOT DEPLOY THAT)
-		new Thread(new Runnable() {
-
-			@Override
-			public void run() {
-				// TODO Auto-generated method stub
-				new DirectoryServer().launch();
-			}
-		}).start();
-
-		new Thread(new Runnable() {
-
-			@Override
-			public void run() {
-				// TODO Auto-generated method stub
-				try {
-					Thread.sleep(1000);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				System.out.println("client");
-				ClientDevice d = new ClientDevice("toto@21", "192.168.1.1",
-						1536);
-				d.createGroup("group1");
-				System.out.println("client");
-
-				System.out.println("done");
-				new ClientDevice("alice@1", "192.168.1.2", 1536)
-						.joinGroup("group1");
-				new ClientDevice("bob@4", "192.168.1.3", 1536)
-						.joinGroup("group1");
-				d.quitGroup("group1");
-				d.close();
-			}
-		}).start();
-
-		try {
-			Thread.sleep(4000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}*/
-	}
+		//new DirectoryServer().launch();
+	//}
 	
 }
