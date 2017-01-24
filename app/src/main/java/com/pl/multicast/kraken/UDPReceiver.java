a4c9d0e6b01e84d8a6f141bbf18ce8b270d069d6
kenny <Kenrabari@yahoo.fr>
2017-01-24 18:07:41 +0100

    UDPReceiver: communication to the broadcaster

--- app/src/main/java/com/pl/multicast/kraken/UDPReceiver.java
+++ app/src/main/java/com/pl/multicast/kraken/UDPReceiver.java
@@ -1,5 +1,15 @@
 package com.pl.multicast.kraken;
 
+import android.bluetooth.BluetoothClass;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 import datum.DeviceData;
 
 /**
@@ -15,10 +25,27 @@ public class UDPReceiver {
         launched = false;
     }
 
-    public void LaunchedReceiver(DeviceData d){
+    public void LaunchedReceiver(){
 
     }
 
-    public void SendMessage(String s){
+    public void SendMessage(DeviceData d, String str){
+        char[] buf = new char[1024];
+        try {
+            Log.i("Group","connection to " + d.getAddr() + ":" + d.getPort());
+            Socket s = new Socket(d.getAddr(), d.getPort());
+
+            PrintWriter writer = new PrintWriter(new OutputStreamWriter(s.getOutputStream()));
+            BufferedReader reader = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            writer.write(str);
+            writer.flush();
+
+            String rstring = reader.readLine();
+            Log.i("Group","msg: " + rstring);
+            //if(rstring.equals(BroadcastService.ACK))
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
