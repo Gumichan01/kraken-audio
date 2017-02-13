50671ae2312c0e8d94ca496aa56d5e54fb33f2c3
Luxon JEAN-PIERRE <luxon.jean.pierre@gmail.com>
2017-02-13 14:32:14 +0100

    Hackojo: deprecated runOperation and defined Hackojo as an asynchrounous task

--- app/src/main/java/com/pl/multicast/kraken/Hackojo.java
+++ app/src/main/java/com/pl/multicast/kraken/Hackojo.java
@@ -1,6 +1,7 @@
 package com.pl.multicast.kraken;
 
 
+import android.os.AsyncTask;
 import android.util.Log;
 
 import com.pl.multicast.kraken.clt.ClientDevice;
@@ -14,8 +15,9 @@ import java.util.List;
 /**
  * Created by Luxon on 16/01/2017.
  */
-public class Hackojo {
+public class Hackojo extends AsyncTask<Integer,Integer,Boolean> {
 
+    public static final int INVALID_OP = -1;
     public static final int GROUP_OP = 0;
     public static final int DEVICE_OP = 1;
     public static final int JOIN_GROUP_OP = 2;
@@ -34,6 +36,8 @@ public class Hackojo {
     }
 
     // TODO: 13/02/2017 Communication with the hackojo server in an asynchronous task
+
+    @Deprecated
     public void runOperation(final int idop) {
 
         Thread t = new Thread(new Runnable() {
@@ -80,5 +84,50 @@ public class Hackojo {
     public synchronized List<DeviceData> getDevices() {
 
         return ddata;
+    }
+
+    @Override
+    protected Boolean doInBackground(Integer... params) {
+
+        boolean status;
+        int idop = (params != null && params.length > 0 ? params[0] : INVALID_OP);
+
+        if(idop == INVALID_OP) {
+            Log.e(this.getClass().getName(),"Invalid operation identifier" );
+            return false;
+        }
+
+        if (idop == GROUP_OP) {
+            // get the groups
+            gdata = cd.groupList();
+            status = gdata != null;
+        } else if (idop == DEVICE_OP) {
+            // get the devices of a group
+            ddata = cd.deviceList(gname);
+            status = ddata != null;
+        } else if (idop == JOIN_GROUP_OP) {
+            // join a group
+            if (!cd.joinGroup(gname)) {
+                Log.e(this.getClass().getName(),"Cannot join " + gname);
+                status = cd.createGroup(gname);
+            } else
+                status = true;
+        } else if (idop == QUIT_GROUP_OP) {
+            // quit a group
+            status = cd.quitGroup(gname);
+        } else if (idop == CREATE_GROUP_OP) {
+            // create a groups
+            if (!cd.createGroup(gname)){
+                Log.e(this.getClass().getName(),"Cannot create " + gname);
+                status = cd.joinGroup(gname);
+            } else
+                status = true;
+        }
+        else{
+            Log.e(this.getClass().getName(),"Invalid operation identifier" );
+            status = false;
+        }
+
+        return status;
     }
 }
