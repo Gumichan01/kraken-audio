ac94ca1eb6d7e25e1ca445d6a3d4c560e0464719
Luxon JEAN-PIERRE <luxon.jean.pierre@gmail.com>
2017-05-08 16:20:55 +0200

    audio: fix bug — AudioTrack

--- app/src/main/java/com/pl/multicast/kraken/KrakenAudio.java
+++ app/src/main/java/com/pl/multicast/kraken/KrakenAudio.java
@@ -5,62 +5,65 @@ import android.media.AudioManager;
 import android.media.AudioTrack;
 import android.util.Log;
 
+import java.util.Arrays;
+
 /**
  * Created by Luxon on 08/05/2017.
  */
 public class KrakenAudio {
 
-    public static final int DATAPCK_SIZE = 1024;
     private KrakenCache kbuffer;
-    private AudioTrack audiotrack;
+    private static AudioTrack audiotrack;
+    private boolean isplaying;
 
     public KrakenAudio() {
         audiotrack = null;
+        isplaying = false;
         kbuffer = new KrakenCache();
     }
 
     public void putInCacheMemory(byte [] arr, int len) {
 
-        Log.i(getClass().getName(), "cache memory");
+        // write into the cache memory
         kbuffer.write(arr,len);
 
-        if(kbuffer.isFull() || !kbuffer.isEmpty()) {
+        if(kbuffer.isFull()) {
 
-            byte [] by = kbuffer.read(DATAPCK_SIZE);
-            System.out.println("recv  — cache");
-            for (byte b : by) {
-                Log.i(getClass().getName(), "" + b);
+            byte [] by = kbuffer.readAll();
+            Log.i(getClass().getName(), "recv  — cache");
+            // blocking write
+            if(audiotrack != null) {
+
+                if(!isplaying) {
+                    Log.i(getClass().getName(), "audio  — play");
+                    audiotrack.play();
+                    isplaying = true;
+                }
+                Log.i(getClass().getName(), "audio  — write");
+                audiotrack.write(by, 0, by.length);
             }
-            System.out.println("recv  — cache end");
         }
-
-        // blocking write
-        if(audiotrack != null)
-            audiotrack.write(arr, 0, len);
     }
 
     public void play(int samplerate, boolean stereo, int duration){
 
+        Log.i(getClass().getName(), "audio  — create");
         audiotrack = new AudioTrack(AudioManager.STREAM_MUSIC,
                 samplerate, (stereo ? AudioFormat.CHANNEL_OUT_STEREO : AudioFormat.CHANNEL_OUT_MONO),
                 AudioFormat.ENCODING_PCM_16BIT, (duration * samplerate),
                 AudioTrack.MODE_STREAM);
-
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                audiotrack.play();
-            }
-        }).start();
     }
 
     public void stop() {
 
+        Log.i(getClass().getName(), "audio  — stop");
         audiotrack.stop();
+        isplaying = false;
     }
 
     public void clearAudio() {
 
+        Log.i(getClass().getName(), "audio  — release");
         audiotrack.pause();
         audiotrack.flush();
     }
